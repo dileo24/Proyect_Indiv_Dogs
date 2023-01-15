909d63cf8dc95577c8a401b91ceaeb886027450e
dileo24 <joacodileo@gmail.com>
2023-01-15 19:12:56 -0300

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import { Route } from "react-router-dom"
-import LandingPage from './components/LandingPage/LandingPage'
-import Home from './components/Home/Home';
-import DogCreate from './components/DogCreate/DogCreate';
-import Detail from './components/Detail/Detail';
+import "./App.css";
+import { Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage";
+import Home from "./components/Home/Home";
+import DogCreate from "./components/DogCreate/DogCreate";
+import Detail from "./components/Detail/Detail";
 
 import axios from "axios";
 
-//axios.defaults.baseURL = 'http://localhost:3001';//servidor local
+axios.defaults.baseURL = "http://localhost:3001"; //servidor local
 //axios.defaults.baseURL = 'https://deploy-production-84fc.up.railway.app/';//servidor deployado
 
 function App() {
   return (
     <div className="App">
-      <Route exact path='/' component={LandingPage} />
-      <Route exact path='/home' component={Home} />
-      <Route path='/home/:id' component={Detail} />
-      <Route path='/dog' component={DogCreate} />
+      <Route exact path="/" component={LandingPage} />
+      <Route exact path="/home" component={Home} />
+      <Route path="/home/:id" component={Detail} />
+      <Route path="/dog" component={DogCreate} />
     </div>
   );
 }
